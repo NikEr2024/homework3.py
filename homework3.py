ce380388f42eb8f82dd6b5f5f459cd07c195ff11
NikEr2024 <n.erdyneev2000@gmail.com>
2024-05-26 14:39:27 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,11 @@
+name = 'Nickolas'
+print ('Name: ' + name)
+
+age =  23
+print('Age: ', (int(age)))
+
+New_age = age + 1
+print('New age: ', + New_age)
+
+is_student = True
+print("Is student:True")
